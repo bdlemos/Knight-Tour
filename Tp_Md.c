a4a0b59a250eb62140861fb65bd8fe0ee548f14f
bdlemos <bdutralemos@gmail.com>
2022-09-09 23:53:04 -0300

    Versao funcional completa

--- Tp_Md.c
+++ Tp_Md.c
@@ -3,35 +3,75 @@
 #define SIZE 8
 
 // Definindo as variaveis globais
-int condition = 0;
-int rep = 0;
-int retrocessos = 0;
-int move_count = 0;
-int board[SIZE][SIZE];
-int moves[2][SIZE * SIZE];
-int Moves[2][8] = {
-    {-1, -2, -2, -1, 1 ,2, 2, 1}, 
+int condition; // Condiçao de parada da recursao;
+int rep; // Numero de repetições
+int retrocessos; // Numero de retrocessos
+int move_count; // Numero de movimentos validos
+int board[SIZE][SIZE]; // Tabuleiro
+int qnt_moves[2][8];// Armazena a quantidade de movimento futuro para cada um dos possiveis movimentos atuais, onde linha 0 é valor e a linha 1 os indices
+int Moves[2][8] = { //Armazena Todos os movimentos possiveis onde a linha 0 é a posiçao x e a linha 1 o y
+    {-1, -2, -2, -1, 1, 2, 2, 1}, 
     {2, 1, -1, -2, -2, -1, 1, 2}
-};
+}; 
 
-// Declaração de funçoes
-// Funçoes M1 a M8 sao responsaveis por verificar se o movimento eh possivel
-//Vou refatorar a logica dos movimentos
-//int possible moves(int i, int j);
-int IsValid(int i, int j);
-void add_move(int i, int j);
-void remove_move(int i, int j);
-void possible_moves(int i, int j);
+// Declaraçao de Funçoes
+
+// Retorna a quantidade de movimentos possiveis para a posição de entrada
+int possible_moves(int i, int j); 
+// Retorna verdadeiro se a posição for valida, caso contrario, falso.
+int IsValid(int i, int j); 
+// Adiciona movimento
+void add_move(int i, int j); 
+// Remove movimento
+void remove_move(int i, int j); 
+// Faz o passeio do cavalo recursivamente
+void KnightTour(int i, int j); 
+// Organiza a matriz "qnt_moves" mantendo os valores fieis aos indices
+void bubble_sort(); 
+// Seta as variaveis para iniciar o passeio
+void initialize_board(); 
+// Printa o Tabuleiro
+void print_board(); 
+
 
 
 int main(){
     
+    initialize_board();
+    KnightTour(0,0);
+
+    
+    print_board();
+    printf("O numero de casas visitadas foi %d \nO numero de movimentos retrocedidos foi %d\n", rep, retrocessos);
+    printf("\n\n");
+
+    initialize_board();
+    KnightTour(3,3);
+
+    print_board();
+    printf("O numero de casas visitadas foi %d \nO numero de movimentos retrocedidos foi %d\n", rep, retrocessos);
+    
+
+    return 0;
+}
+
+void initialize_board(){
+    condition = 0;
+    rep = 0;
+    retrocessos = 0;
+    move_count = 0;
+    for (int i = 0; i < 8; i++){
+        qnt_moves[0][i] = 0;
+        qnt_moves[1][i] = i;
+    }
+    
     for (int i = 0; i < SIZE; i++)
         for (int j = 0; j < SIZE; j++)
             board[i][j] = 0;
-    add_move(0,0);
-    possible_moves(0,0);
-    printf("O numero de casas visitadas foi %d \nO numero de movimentos retrocedidos foi %d\n", rep, retrocessos);
+    
+}
+
+void print_board(){
     for (int i = 0; i < SIZE; i++){
         for (int j = 0; j < SIZE ; j++){
             if(board[i][j] < 10){
@@ -42,40 +82,65 @@ int main(){
         }
         printf("\n");
     }
-    return 0;
 }
 
 int IsValid(int i, int j){
-    if ((i >= SIZE || i < 0) || (j >= SIZE || j < 0) || (board[i][j] != 0)){
-        return 0;
+    if ((i < SIZE && i >= 0) && (j < SIZE && j >= 0) && (board[i][j] == 0)){
+        return 1;
     }
-    return 1;
+    return 0;
 }
 
 void add_move(int i, int j){
     board[i][j] = move_count + 1;
-    moves[0][move_count] = i;
-    moves[1][move_count] = j;
     move_count++;
 }
 
 void remove_move(int i, int j){
     retrocessos++;
     board[i][j] = 0;
-    moves[0][move_count] = 0;
-    moves[1][move_count] = 0;
     move_count--;
 }
 
-void possible_moves(int i, int j){
-    rep++;
-    if (move_count == SIZE * SIZE)
-        condition = 1; 
-
+int possible_moves(int i, int j){
+    int cont = -1;
     for (int k = 0; k < 8; k++){
         if (IsValid(i + Moves[0][k], j + Moves[1][k])){
-            add_move(i + Moves[0][k], j + Moves[1][k]);
-            possible_moves(i + Moves[0][k], j + Moves[1][k]);
+            cont++;
+        }
+    }
+    return cont;
+}
+
+void bubble_sort(){
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            if (qnt_moves[0][i] < qnt_moves[0][j]){
+                int aux = qnt_moves[1][j];
+                qnt_moves[1][j] = qnt_moves[1][i];
+                qnt_moves[1][i] = aux;
+                int aux2 = qnt_moves[0][j];
+                qnt_moves[0][j] = qnt_moves[0][i];
+                qnt_moves[0][i] = aux2;
+            }
+        }
+    }
+}
+
+void KnightTour(int i, int j){
+    add_move(i, j);
+    rep++;
+    if (move_count == SIZE * SIZE)
+        condition = 1;
+
+    for (int k = 0; k < 8; k++){
+        qnt_moves[0][k] = possible_moves(i + Moves[0][k], j + Moves[1][k]);
+        qnt_moves[1][k] = k;
+    }
+    bubble_sort();
+    for (int k = 0; k < 8; k++){
+        if (IsValid(i + Moves[0][qnt_moves[1][k]], j + Moves[1][qnt_moves[1][k]])){
+            KnightTour(i + Moves[0][qnt_moves[1][k]], j + Moves[1][qnt_moves[1][k]]);
             if(condition) return;
         }
 
