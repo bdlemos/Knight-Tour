405f45775ee8902774d494c5adac8cf8e36eeb32
bdlemos <bdutralemos@gmail.com>
2022-10-23 10:47:22 -0300

    Versão final completa

--- FinalVersion/KnightTour.h
+++ FinalVersion/KnightTour.h
@@ -12,7 +12,6 @@ int rep; // Numero de repetições
 int regression; // Numero de regression
 int move_count; // Numero de movimentos validos
 int board[SIZE][SIZE]; // Tabuleiro
-int qnt_moves[2][8];// Armazena a quantidade de movimento futuro para cada um dos possiveis movimentos atuais, onde linha 0 é valor e a linha 1 os indices
 int Moves[2][8] = { //Armazena Todos os movimentos possiveis onde a linha 0 é a posiçao x e a linha 1 o y
     {-1, -2, -2, -1, 1, 2, 2, 1}, 
     {2, 1, -1, -2, -2, -1, 1, 2}
@@ -30,14 +29,16 @@ void add_move(int i, int j);
 void remove_move(int i, int j); 
 // Faz o passeio do cavalo recursivamente
 void KnightTour(int i, int j); 
-//Troca os valores
+// Troca os valores
 void swap(int* a,int* b);
 // Organiza a matriz "qnt_moves" mantendo os valores fieis aos indices
-void bubble_sort(); 
+void bubble_sort(int **qnt_moves); 
 // Seta as variaveis para iniciar o passeio
 void initialize_board(); 
 // Printa o Tabuleiro
 void print_board(); 
+// Faz o papel de "main"
+void passeio(int i, int j);
 
 
 
@@ -47,11 +48,11 @@ void passeio(int i, int j){
     initialize_board();
     KnightTour(i,j);
 
-    FILE* f = fopen("KnightTour.txt", "a");
+    FILE* f = fopen("saida.txt", "a");
     if (f == NULL) return;
+
     print_board(f);
-    fprintf(f, "O numero de casas visitadas foi %d \nO numero de movimentos retrocedidos foi %d\n", rep, regression);
-    fprintf(f, "\n\n");
+    fclose(f);
 }
 
 void initialize_board(){
@@ -59,11 +60,6 @@ void initialize_board(){
     rep = 0;
     regression = 0;
     move_count = 0;
-    for (int i = 0; i < 8; i++){
-        qnt_moves[0][i] = 0;
-        qnt_moves[1][i] = i;
-    }
-    
     for (int i = 0; i < SIZE; i++)
         for (int j = 0; j < SIZE; j++)
             board[i][j] = 0;
@@ -74,12 +70,14 @@ void print_board(FILE *f){
     for (int i = 0; i < SIZE; i++){
         for (int j = 0; j < SIZE ; j++){
             fprintf(f, "%d", board[i][j]);
-            if (j < SIZE - 1){
+            if (j < SIZE){
                 fprintf(f, " ");
             }
         }
         fprintf(f,"\n");
     }
+    //Printa Casas visitadas Casas retrocedidas
+    fprintf(f, "%d %d\n", rep, regression);
 }
 
 int IsValid(int i, int j){
@@ -90,8 +88,7 @@ int IsValid(int i, int j){
 }
 
 void add_move(int i, int j){
-    board[i][j] = move_count + 1;
-    move_count++;
+    board[i][j] = ++move_count;
 }
 
 void remove_move(int i, int j){
@@ -116,7 +113,7 @@ void swap(int* a, int* b){
     *b = aux;
 }
 
-void bubble_sort(){
+void bubble_sort(int **qnt_moves){
     for (int i = 0; i < 8 - 1; i++){
         for (int j = 0; j < 8 - 1 - i; j++){
             if (qnt_moves[0][j + 1] < qnt_moves[0][j]){
@@ -130,24 +127,32 @@ void bubble_sort(){
 void KnightTour(int i, int j){
     add_move(i, j);
     rep++;
+    
+    // qnt_moves = Armazena a quantidade de movimento futuro para cada um dos possiveis movimentos atuais, onde linha 0 é valor e a linha 1 os indices
+    int **qnt_moves = (int**) malloc(2*sizeof(int*));
+    for (int i = 0; i < 2; i++) qnt_moves[i] = (int*) malloc(8*sizeof(int));
+    
     if (move_count == SIZE * SIZE)
         condition = 1;
-
+    // Realiza a heuristica de prioridade de movimentos
     for (int k = 0; k < 8; k++){
         qnt_moves[0][k] = possible_moves(i + Moves[0][k], j + Moves[1][k]);
         qnt_moves[1][k] = k;
     }
-    bubble_sort();
+    bubble_sort(qnt_moves);
+    //Testa os caminhos possíveis 
     for (int k = 0; k < 8; k++){
         if (IsValid(i + Moves[0][qnt_moves[1][k]], j + Moves[1][qnt_moves[1][k]])){
             KnightTour(i + Moves[0][qnt_moves[1][k]], j + Moves[1][qnt_moves[1][k]]);
-            if(condition) return;
+            if(condition) break;
         }
 
     }
-    
-    if (!condition)
+    //Caso o código cheque nesse ponto e a condição seja negativa, realiza o backtracking
+    if (!condition){
         remove_move(i,j);
+    }
+    for (int i = 0; i < 2; i++) free(qnt_moves[i]);free(qnt_moves);
     return;
 }
 
--- FinalVersion/main.c
+++ FinalVersion/main.c
@@ -2,6 +2,10 @@
 #include "KnightTour.h"
 
 int main(){
-    passeio(1,4);
+   for (int i = 1; i < 9; i++){
+        for (int j = 1; j < 9; j++){
+            passeio(i,j);
+        }
+    }
     return 0;
 }
